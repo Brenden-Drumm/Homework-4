d5ec239b493eb1bf647079b691e283ff02ab5b8f
Brenden-Drumm <158791171+Brenden-Drumm@users.noreply.github.com>
2024-04-05 22:27:50 -0400

    Add files via upload

--- SocialMediaAccount.cpp
+++ SocialMediaAccount.cpp
@@ -1,6 +1,21 @@
 #include "SocialMediaAccount.h"
 
-SocialMediaAccount::SocialMediaAccount()
-{
-    //ctor
-}
+template<class T>
+SocialMediaAccount::SocialMediaAccount(bool privateCheck, T hndlr, int followerCnt, int followedCnt):
+privacy(privateCheck), handler(hndlr), followerCount(followerCnt), followedCount(followedCnt){}
+
+template<class T>
+bool SocialMediaAccount::getPriv()const{return privacy;}
+void SocialMediaAccount::setPriv(bool priv){privacy = priv;}
+
+bool SocialMediaAccount::getFollowed()const{return followedCount;}
+void SocialMediaAccount::setFollowed(bool followed){followedCount = followed;}
+
+bool SocialMediaAccount::getFollowers()const{return followerCount;}
+void SocialMediaAccount::setFollowers(bool follower){followerCount = follower;}
+
+void SocialMediaAccount::displayFollowers(){}
+void SocialMediaAccount::displayFollowed(){}
+
+void addFollower (T user);
+void addFollowed (T user);
--- SocialMediaAccount.h
+++ SocialMediaAccount.h
@@ -1,15 +1,28 @@
 #ifndef SOCIALMEDIAACCOUNT_H
 #define SOCIALMEDIAACCOUNT_H
 
-
+template<class T>
 class SocialMediaAccount
 {
     public:
         SocialMediaAccount();
-
-    protected:
+        SocialMediaAccount(bool privateCheck, T hndlr, int followerCnt, int followedCnt);
+        bool getPriv()const;
+        void displayFollowers();
+        void displayFollowed();
+        void addFollower (T user);
+        void addFollower (T user);
 
     private:
+        T handler;//owner
+        T followers;//[int maxFollowers];
+        T followed;//[int maxFollowed];
+        int followerCount; //number of followers.
+        int followedCount; //number of following.
+        bool privacy;//Determines whether account is private or not.
 };
+
+const int maxFollowers = 10;
+const int maxFollowed = 10;
 
 #endif // SOCIALMEDIAACCOUNT_H
